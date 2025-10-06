28a6f3dc2864aa41c9c975e05a2a59f3dcc83d14
Brady Richardson <bradyrr33@gmail.com>
2025-10-06 11:27:59 -0600

    fix update layout page to accomodate route changes

--- frontend/bam_frontend/src/App.tsx
+++ frontend/bam_frontend/src/App.tsx
@@ -6,7 +6,13 @@ import { route } from "./utils/route/route";
 function App() {
   const [us_selectedChild, us_setSelectedChild] = useState<string>("search");
 
-  return <Layout children={route(us_selectedChild)} />;
+  const selectedPage = route(us_selectedChild);
+
+  return (
+    selectedPage && (
+      <Layout header={selectedPage.header} body={selectedPage.body} />
+    )
+  );
 }
 
 export default App;
